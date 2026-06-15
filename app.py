8d637b51b288071a43640740bcc197c20d273a11
MD ANZAR KHAN <144884339+pathan172@users.noreply.github.com>
2026-06-16 04:50:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from ui_components import (
     page_header, render_analytics_section, render_activity_section,
     render_suggestions_section
 )
-from feedback.feedback import FeedbackManager
+#from feedback.feedback import FeedbackManager
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.shared import Inches, Pt
 from docx import Document
